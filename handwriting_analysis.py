53cfca610037f8a8b5dc1e16178a1d8238fae67b
Barkyeongchan <xodudcksxo@gmail.com>
2025-09-03 16:17:36 +0900

    추가 수정 및 작동 테스트

--- handwriting_analysis.py
+++ handwriting_analysis.py
@@ -14,10 +14,26 @@ def predict_handwriting(img_path):
 
     # 예측
     pred = model.predict(x)[0][0]
-    if pred > 0.5:
-        print(f"{img_path}: 내 글씨")
+
+    # 퍼센티지 계산
+    my_percent = (1 -pred) * 100
+    other_percent = pred * 100
+
+    # 출력
+    if my_percent > other_percent:
+        print(f"{img_path}: 내 글씨 ({my_percent:.2f}%) / 다른 사람 글씨 ({other_percent:.2f}%)")
     else:
-        print(f"{img_path}: 다른 사람 글씨")
+        print(f"{img_path}: 다른 사람 글씨 ({other_percent:.2f}%) / 내 글씨 ({my_percent:.2f}%)")
+
 
 # ================= 실행 =================
-predict_handwriting("./test.png")
+predict_handwriting("./img/test_1.png")
+predict_handwriting("./img/test_2.png")
+predict_handwriting("./img/test_3.png")
+predict_handwriting("./img/test_4.png")
+predict_handwriting("./img/test_5.png")
+predict_handwriting("./img/test_6.png")
+predict_handwriting("./img/test_7.png")
+predict_handwriting("./img/test_8.png")
+predict_handwriting("./img/test_9.png")
+predict_handwriting("./img/test_10.png")
